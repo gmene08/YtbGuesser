2dd5ee86fcb8b3a61ec8096b7ce4fb63df191207
gmene08 <gab.menegati@gmail.com>
2026-05-27 12:37:37 -0400

    feat: implement JWT handshake interceptor

--- video-guesser-api/src/main/java/com/gabmene/videoguesser/config/WebSocketConfig.java
+++ video-guesser-api/src/main/java/com/gabmene/videoguesser/config/WebSocketConfig.java
@@ -13,7 +13,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("http://localhost:4200")
+                .addInterceptors(new JwtHandshakeInterceptor());
     }
 
     @Override
