b3526632124fc74f6b12e1c2b988b440e01656fd
gmene08 <gab.menegati@gmail.com>
2026-06-15 10:34:20 -0400

    feat: add Redis integration for activity log management

---
+++ videoguesser-engine/src/services/logService.js
@@ -0,0 +1,40 @@
+const {redisClient} = require('../config/redis');
+
+async function addActivityLog(roomCode, io, type, logMessage) {
+    const logKey = `room:${roomCode}:logs`;
+    const timestamp = new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+    const logEntry = { timestamp, type, message: logMessage };
+    try{
+        await redisClient.lPush(logKey, JSON.stringify(logEntry));
+
+        await redisClient.expire(logKey, 7200); // Expira em 2 horas para evitar acúmulo de logs antigos
+
+        io.to(`${roomCode}-game`).emit('newLogEntry', logEntry);
+    }
+    catch(err){
+        console.error(`❌ Erro ao adicionar log da sala [${roomCode}] no Redis:`, err.message);
+    }
+
+}
+
+async function getActivityLogs(roomCode) {
+    const logKey = `room:${roomCode}:logs`;
+    try {
+        const logs = await redisClient.lRange(logKey, 0, -1);
+        return logs.map(log => JSON.parse(log)).reverse(); // Retorna do mais antigo para o mais recente
+    } catch (err) {
+        console.error(`❌ Erro ao buscar logs da sala [${roomCode}] no Redis:`, err.message);
+        return [];
+    }
+}
+
+async function clearActivityLogs(roomCode) {
+    const logKey = `room:${roomCode}:logs`;
+    try {
+        await redisClient.del(logKey);
+    } catch (err) {
+        console.error(`❌ Erro ao limpar logs da sala [${roomCode}] no Redis:`, err.message);
+    }
+}
+
+module.exports = { addActivityLog, getActivityLogs, clearActivityLogs };
